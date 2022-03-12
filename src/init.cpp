8379f9caac2606a66eb819526e1013ca65d7ca02
Avery Allison <averyallison02@gmail.com>
2022-03-11 19:03:56 -0800

    added random function and some comments

--- src/init.cpp
+++ src/init.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
+/*
+function doRand
+returns a random number from 0 to 99 for other functions to use
+*/
+int doRand() {
+
+    srand(time(NULL));
+    return rand() % 100;
+}
+
+
+// main function
 int main() {
 
     cout << "Main function established." << endl;
